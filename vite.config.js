23ce71908c3016f112389e1349605905638e6912
Navs.Devs <rogelnavarro74@gmail.com>
2025-09-13 01:40:42 +0800

    base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Expense-Tracker/",
+  base: "./",   // ✅ relative path works for Vercel
   server: {
     open: true,
     port: 5173,
