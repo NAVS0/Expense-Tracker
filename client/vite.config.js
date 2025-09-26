3059879aa2a3ab812cec69e7e917bafbc414f0cc
Navs.Devs <rogelnavarro74@gmail.com>
2025-09-26 15:09:55 +0800

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Expense-Tracker/", // <-- Add this
   server: {
     proxy: {
-      "/api": "http://localhost:5001", // proxy API calls to backend
+      "/api": "http://localhost:5001",
     },
   },
-})
+});
